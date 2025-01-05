11cd0e6b6607c59f265ff1478fbbbf482a3f7bc9
mic <m@micz.it>
2025-01-05 23:52:24 +0100

    correctly using this.llmName

--- api_webchat/messagesArea.js
+++ api_webchat/messagesArea.js
@@ -174,7 +174,7 @@ class MessagesArea extends HTMLElement {
 
         if (isLastMessageFromUser) {
             const header = document.createElement('h2');
-            header.textContent = "Chat GPT" + (type=='error' ? " - Error" : "");
+            header.textContent = this.llmName + (type=='error' ? " - Error" : "");
             this.messages.appendChild(header);
         }
 
