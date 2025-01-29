1e74829d2649e732f11191a3562192c7045af0dd
mic <m@micz.it>
2025-01-29 22:24:47 +0100

    added an error message if sparks is not installed

--- js/mzta-menus.js
+++ js/mzta-menus.js
@@ -279,11 +279,17 @@ export class mzta_Menus {
                             return {ok:'0'};
                         }
                         this.logger.log("calendar_event_data: " + calendar_event_data);
-                        let result_openCalendarEventDialog = await browser.runtime.sendMessage('thunderai-sparks@micz.it',{action: "openCalendarEventDialog", calendar_event_data: calendar_event_data})
-                        if(result_openCalendarEventDialog == 'ok'){
-                            return {ok:'1'};
-                        } else {
-                            browser.tabs.sendMessage(tabs[0].id, { command: "sendAlert", curr_tab_type: tabs[0].type, message: browser.i18n.getMessage("calendar_opening_dialog_error") + ": " + result_openCalendarEventDialog.error });
+                        try{
+                            let result_openCalendarEventDialog = await browser.runtime.sendMessage('thunderai-sparks@micz.it',{action: "openCalendarEventDialog", calendar_event_data: calendar_event_data})
+                            if(result_openCalendarEventDialog == 'ok'){
+                                return {ok:'1'};
+                            } else {
+                                browser.tabs.sendMessage(tabs[0].id, { command: "sendAlert", curr_tab_type: tabs[0].type, message: browser.i18n.getMessage("calendar_opening_dialog_error") + ": " + result_openCalendarEventDialog.error });
+                                return {ok:'0'};
+                            }
+                        }catch(err){
+                            console.error("[ThunderAI] Error opening calendar event dialog: ", JSON.stringify(err));
+                            browser.tabs.sendMessage(tabs[0].id, { command: "sendAlert", curr_tab_type: tabs[0].type, message: browser.i18n.getMessage("calendar_opening_dialog_error") + ": " + "Sparks not installed!" });
                             return {ok:'0'};
                         }
                         break;  // Get a calendar event info - END
