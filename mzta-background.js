127f3ab18b968e69712f42308a27ad43ca39122f
mic <m@micz.it>
2025-03-03 21:23:37 +0100

    unchecking add_tags if permissions revoked. see #276

--- mzta-background.js
+++ mzta-background.js
@@ -798,4 +798,12 @@ const newEmailListener = (folder, messagesList) => {
     return _newEmailListener();
 }
 
+// Disable tag options if permissions are not granted
+if (prefs_init.add_tags) {
+    if(!await browser.permissions.contains({permissions: ["messagesMove", "messagesTagsList", "messagesTags", "messagesUpdate"]})){
+        browser.storage.sync.set({add_tags: false});
+        taLog.log("Permissions not granted, disabling add_tags");
+    }
+}
+
 browser.messages.onNewMailReceived.addListener(newEmailListener, !prefs_init.add_tags_auto_only_inbox);
