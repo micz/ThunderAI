e0f5f263edb6b9fc29b01ab4aad455fdd7e9e56c
mic <m@micz.it>
2025-12-22 23:46:58 +0100

    model filtering improved

--- js/api/openai.js
+++ js/api/openai.js
@@ -65,7 +65,7 @@ export class OpenAI {
       let output = {};
       output.ok = true;
       let output_response = await response.json();
-      output.response = output_response.data.filter(item => item.id.startsWith('gpt-')).sort((a, b) => b.id.localeCompare(a.id));
+      output.response = output_response.data.filter(item => item.id.startsWith('gpt-') || item.id.startsWith('o1-') || item.id.startsWith('o4-') || item.id.startsWith('o3-')).sort((a, b) => b.id.localeCompare(a.id));
 
       return output;
     }catch (error) {
