b32ce4f756a609db6b1a864519a98e0fe903532c
mic <m@micz.it>
2026-01-03 23:04:01 +0100

    copyright date updated to 2026

--- js/api/openai_comp.js
+++ js/api/openai_comp.js
@@ -1,6 +1,6 @@
 /*
  *  ThunderAI [https://micz.it/thunderbird-addon-thunderai/]
- *  Copyright (C) 2024 - 2025  Mic (m@micz.it)
+ *  Copyright (C) 2024 - 2026  Mic (m@micz.it)
 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
