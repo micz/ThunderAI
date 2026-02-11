c29b4b335295da9929b88a835a43534fa074bb77
mic <m@micz.it>
2026-02-11 21:45:48 +0100

    userInput is now an array. see #525 #554

--- api_webchat/controller.js
+++ api_webchat/controller.js
@@ -262,7 +262,7 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
             }
             break;
         case 'api_send_custom_text':
-            let userInput = message.custom_text;
+            let userInput = message.custom_text;    // From version 4.0.0 this is an array
                 if(userInput !== null) {
                     if(!placeholdersUtils.hasPlaceholder(promptData.prompt, 'additional_text')){
                         // no additional_text placeholder, do as usual
@@ -270,7 +270,14 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
                     }else{
                         // we have the additional_text placeholder, do the magic!
                         let finalSubs = {};
-                        finalSubs["additional_text"] = userInput;
+                        
+                        if (Array.isArray(userInput)) {
+                            userInput.forEach(obj => {
+                                finalSubs[obj.placeholder.replace(/^{%|%}$/g, '').trim()] = obj.custom_text;
+                            });
+                        } else {
+                            finalSubs["additional_text"] = userInput;
+                        }
                         promptData.prompt = placeholdersUtils.replacePlaceholders({
                             text: promptData.prompt,
                             replacements: finalSubs,
